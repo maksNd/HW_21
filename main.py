97fbbfd2e24ebce8dae027a3c9aed6afca2091b5
mkN <mknord1565@gmail.com>
2023-01-21 19:30:21 +0600

    improvements

--- main.py
+++ main.py
@@ -7,38 +7,42 @@ store = Store({'печеньки': 3, 'собачки': 4, 'коробки': 5},
 shop = Shop({'собачки': 2}, name='магазин')
 points = [shop, store]
 
-user_input = input('Tap ENTER or input your request\n')
-if user_input == '':
-    request = Request('Доставить 3 собачки из склад в магазин')
-    print(request.get_source_string())
-else:
-    request = Request(user_input)
+user_input = None
+while True:
+    user_input = input('\nTap ENTER or "stop" or input your request\n')
+    if user_input.lower() == 'stop':
+        quit(print('Good luck!'))
+    if user_input == '':
+        request = Request('Доставить 3 собачки из склад в магазин')
+        print(request.get_source_string())
+    else:
+        request = Request(user_input)
 
-destination_point = None
-point_from = None
+    destination_point = None
+    point_from = None
 
-try:
-    for point in points:
-        if request.to_place == point:
-            destination_point = point
-        if request.from_place == point:
-            point_from = point
-except AttributeError:
-    quit(print('Некорректный запрос'))
+    try:
+        for point in points:
+            if request.to_place == point:
+                destination_point = point
+            if request.from_place == point:
+                point_from = point
+    except AttributeError:
+        quit(print('Некорректный запрос'))
 
-if point_from.check_availability(request.product_title, request.product_amount):
-    sleep(1)
-    print('Нужное количество есть на складе')
-    sleep(1)
-    point_from.remove(request.product_title, request.product_amount)
-    destination_point.add(request.product_title, request.product_amount)
-    print(f'Курьер забрал {request.product_amount} {request.product_title} из {point_from}')
-    sleep(1)
-    print(f'Курьер доставил {request.product_amount} {request.product_title} в {destination_point}')
-    print()
-    sleep(1)
-    print(f'{str(destination_point).title()} хранит: {destination_point.get_items()}\n')
-    sleep(1)
-    print(f'{str(point_from).title()} хранит: {point_from.get_items()}')
-else:
-    print('Но нужного количества нет')
+    if point_from.check_availability(request.product_title, request.product_amount):
+        sleep(1)
+        print('Нужное количество есть на складе')
+        sleep(1)
+        point_from.remove(request.product_title, request.product_amount)
+        destination_point.add(request.product_title, request.product_amount)
+        print(f'Курьер забрал {request.product_amount} {request.product_title} из {point_from}')
+        sleep(1)
+        print(f'Курьер доставил {request.product_amount} {request.product_title} в {destination_point}')
+        print()
+        sleep(1)
+        print(f'{str(destination_point).title()} хранит: {destination_point.get_items()}\n')
+        sleep(1)
+        print(f'{str(point_from).title()} хранит: {point_from.get_items()}')
+    else:
+        print('Но нужного количества нет')
